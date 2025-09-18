24e126abc88b5d0684c576be12619079b4948565
Guardianff <103484627+Guardianff@users.noreply.github.com>
2025-09-18 12:26:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     API_ID = environ.get("API_ID", "27485646")
     API_HASH = environ.get("API_HASH", "60d9b4cac575e735e25c1a00885e91d9")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7334566762:AAGh_zrIbGTMyysKoxPccUcHWftu2bJe9rE") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6124951040').split()]
+    #BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6124951040').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     
@@ -30,4 +30,5 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
