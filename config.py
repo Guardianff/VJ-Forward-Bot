4c0fb7ce4efd2a72bcb9ead6b87c182739a2db29
Guardianff <103484627+Guardianff@users.noreply.github.com>
2025-09-29 09:18:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "27485646")
-    API_HASH = environ.get("API_HASH", "60d9b4cac575e735e25c1a00885e91d9")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7334566762:AAGh_zrIbGTMyysKoxPccUcHWftu2bJe9rE") 
+    API_ID = environ.get("API_ID", "")
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7334566762:") 
     #BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6124951040').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://AMMAAAAA:AMMAAAAA@cluster0.mi7ldio.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Forward")
-    BOT_OWNER = int(environ.get("BOT_OWNER", "6124951040"))
+    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
@@ -30,6 +30,7 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
